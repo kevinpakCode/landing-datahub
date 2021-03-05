6f02dece7966b464de55104bb07832c403f2c4b6
Kevinpak <kevinpak@yandex.ru>
2021-03-05 20:04:07 +0300

    [FRN-157] add min container

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0b40c9eff38ce705b94b";
+/******/ 	var hotCurrentHash = "6c3e6520673480f9473c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
