bf71c42393a20c8646fa70a266c46b294588dc14
Kevinpak <kevinpak@yandex.ru>
2021-04-05 18:42:07 +0300

    [FRN-202] form submit

--- components/form/get-demo.tsx
+++ components/form/get-demo.tsx
@@ -1,10 +1,101 @@
+import {useState, useEffect} from 'react'
+import InputMask from 'react-input-mask'
+import axios from 'axios'
+
 import styles from './../../styles/components/get-demo.module.scss'
 
 const GetDemo = () => {
+  const  [phone, setPhone] = useState('')
+  const  [phoneBlur, setPhoneBlur] = useState(false)
+  const  [phoneValid, setPhoneValid] = useState(false)
+  const  [phoneError, setPhoneError] = useState(false)
+  const [successRequest, setSuccessRequest] = useState(false)
+
+  const handleChange = e => {
+    const val = e.target.value
+    setPhone(val)
+    setPhoneError(false)
+    console.log(val)
+  }
+
+  const handleBlur = e => {
+    setPhoneBlur(true)
+    console.log(phoneBlur)
+  }
+
+  const handleSubmit = e => {
+    if(phoneBlur&&phone.length) {
+      const newPhone = phone.replace(/([-_()+ ])/g, '')
+      if(newPhone.length === 11) {
+        setPhoneError(false)
+        setPhoneValid(true)
+      } else {
+        setPhoneError(true)
+        setPhoneValid(false)
+      }
+    } else {
+      setPhoneError(true)
+      setPhoneValid(false)
+    }
+  }
+
+
+  useEffect(() => {
+    if(phoneValid) {
+      const dataForm = {
+        data : {
+          name : '',
+          email: '',
+          phone,
+          company: '',
+          text: ''
+        }
+      }
+      //=> Axios, post data
+      axios({
+        method: 'post',
+        url: `${process.env.API_URL}/api/v1/forms`,
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        data: JSON.stringify(dataForm),
+      })
+      .then(function (response) {
+        setPhone('')
+        setSuccessRequest(true)
+        setTimeout(()=> {
+          setSuccessRequest(false)
+        }, 3000)
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+    }
+    
+  }, [phoneValid])
+
+
   return (
-    <div className={styles.getDemo}>
-      <input type="email" placeholder="Телефон"  className={styles.field} name="email"/>
-      <button className={styles.btn}><span>Получить демо</span></button>
+    <div className={`${styles.getDemo} ${phoneError? styles.error:''}`}>
+      <InputMask
+        type="text" 
+        placeholder="Телефон"  
+        className={styles.field} 
+        mask="(+7) 999-999-99-99"
+        name="phone"
+        value={phone}
+        onBlur={handleBlur}
+        onChange={handleChange}
+      >
+      </InputMask>
+      <button className={styles.btn} onClick={handleSubmit}><span>Получить демо</span></button>
+      {
+        successRequest&&
+        <div className={styles.successRequest}>
+          Ваша заявка успешно отправлена!
+        </div>
+      }
     </div>
   )
 }
